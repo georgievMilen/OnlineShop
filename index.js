d0b7c8b6c5969191b6ad50d7ec76e24c30e96baa
Your Name <milengeorgievgm@gmail.com>
2020-04-24 16:36:17 +0300

    index

--- index.js
+++ index.js
@@ -16,19 +16,66 @@ const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env"})
-mongoose.connect('mongodb://localhost/onlineshop', {useNewUrlParser:true})
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err))
 
+// Connect to database
+connectDB();
+
+const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ 
-  extended: true 
-}));
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(cookieParser());
 
+//Route files
+const categories = require("./modules/categories/routes/categories");
+const products = require("./modules/produts/routes/produts");
+const users = require("./modules/users/routes/users");
+const auth = require("./modules/users/routes/auth");
+
+app.use(errorHandler);
+
+// Dev tools / middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 200
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount routers
 app.use('/api/categories', categories);
 app.use('/api/products', products);
-app.use('/api/fileupload', fileupload);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
-const port = process.env.PORT||3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const PORT = process.env.PORT || 3000;
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .yellow.bold
+  )
+);
